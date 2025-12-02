f50b012463ab2f2b7ff9046ab68ac61e941711ce
Faisal-Elhouderi <faisal.ali.172002@gmail.com>
2025-12-02 21:45:19 +0200

    cleaned the first trash data

--- temp_station.m
+++ temp_station.m
@@ -1,5 +1,5 @@
 %% Load and Plot Step Response Data
-data = readtable('data.csv');
+data = readtable('./data/data.csv');
 
 time = data.timestamp_ms / 1000;  % Convert to seconds
 voltage = (data.voltage + 0.15) .*(1.5);
@@ -32,7 +32,7 @@ step_input = setpoint_temp * ones(size(voltage));  % Step input
 id_data = iddata(voltage - y_initial, step_input, Ts);
 
 % Estimate 2nd order transfer function
-sys_est = tfest(id_data, 4);  % 2nd order
+sys_est = tfest(id_data, 3);  % 2nd order
 disp('System ID Toolbox Result:');
 sys_est
 
